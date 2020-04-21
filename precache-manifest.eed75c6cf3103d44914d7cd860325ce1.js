2e6005cb7b52ce2aa3b537093ed0201585d38f0f
Ismail Shaikh <ismailshaikh31097@gmail.com>
2020-04-21 12:34:14 +0530

    Updates

---
+++ precache-manifest.eed75c6cf3103d44914d7cd860325ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc41d66d120d3f0e63f9f726307d069",
+    "url": "/invoicegen/index.html"
+  },
+  {
+    "revision": "1fc7ded20ff4aef6bc53",
+    "url": "/invoicegen/static/css/main.c0769256.chunk.css"
+  },
+  {
+    "revision": "de6b3040c3f48d411661",
+    "url": "/invoicegen/static/js/2.0b132b65.chunk.js"
+  },
+  {
+    "revision": "88011752e4ff9ce31dfb431496cb8e29",
+    "url": "/invoicegen/static/js/2.0b132b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc7ded20ff4aef6bc53",
+    "url": "/invoicegen/static/js/main.04607602.chunk.js"
+  },
+  {
+    "revision": "ce2f10fb8f3030963837",
+    "url": "/invoicegen/static/js/runtime-main.9c546c42.js"
+  },
+  {
+    "revision": "be0366dd619a6e338db28c61b8152b8c",
+    "url": "/invoicegen/static/media/digital_wolf_logo.be0366dd.png"
+  }
+]);
